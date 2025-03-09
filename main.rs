fb8456dbbc9de4a6c2172aae3ca142df8b0656d5
eddiedibella <125487637+eddiedibella@users.noreply.github.com>
2025-03-09 18:55:30 -0400

    main.rs

--- main.rs
+++ main.rs
@@ -1,1 +1,29 @@
+use std::collections::HashMap;
+use std::io::{self, BufRead};
 
+struct Program {
+    lines:Vec<(132, String)>,
+
+}
+fn firstProg() {
+    let stdin = io::stdin();
+    let mut lines:Vec<(132, String)> = stdin
+        .lock()
+        .lines()
+        .filter_map(|line| line.ok())
+        .filter_map(|line| {
+            let space_index = line.find(" ")?;
+            let label = line[..space_index].parse().ok()?;
+            Some((label, line[space_index + 1..].to_string()))
+        })
+        .collect()
+    lines.sort_by_key(|(label, _)| *label);
+    let program = Program {lines};
+    execute_program(program);
+
+}
+
+fn execute_program(program: Program) {
+
+
+}
